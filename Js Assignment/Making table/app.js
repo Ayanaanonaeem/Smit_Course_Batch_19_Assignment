7097fd597a243c3afb94824fd7d45dd16be9da8b
Ayanaanonaeem <ayanaano5@gmail.com>
2026-01-31 17:20:51 +0500

    Table assignment

---
+++ Js Assignment/Making table/app.js
@@ -0,0 +1,11 @@
+var usernum=+prompt("Enter which number table do you want")
+var usernum2=+prompt("From where you want the table")
+
+for(let i=1; i<=usernum2; i++){
+
+    console.log(`${usernum} x ${i} = ${usernum*i}`);
+    
+
+}
+
+
