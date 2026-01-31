320ca583dbe2c4401e0f1b6abbafe62deb07c7d6
Ayanaanonaeem <ayanaano5@gmail.com>
2026-01-31 17:28:57 +0500

    making array by user given values

---
+++ Js Assignment/Making array/app.js
@@ -0,0 +1,13 @@
+var array=[]
+
+
+
+
+for (let i = 0; i < 10; i++) {
+var userdata=prompt("enter something you want to push in arrray and seperate with comma")
+array.push(userdata)
+
+    
+}
+
+console.log(array);
